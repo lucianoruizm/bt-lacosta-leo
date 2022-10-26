296a327c9db8014c0ce99cedffb6e2645cd69041
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-26 11:39:16 -0300

    validate intentos de login

--- scripts/login.js
+++ scripts/login.js
@@ -4,32 +4,34 @@ let formLogin = document.getElementById("formLogin");
 // GET DATOS DEL USUARIO DESDE LOCALSTORAGE
 const userData = JSON.parse(localStorage.getItem("userData")) || [];
 
+// VARIABLE QUE ALMACENA LOS INTENTOS DE INICIO DE SESIÓN
+let intentos = 0;
+
 // FUNCION VALIDACIÓN DE DATOS PARA EL INICIO DE SESIÓN
-function validateLogin(passRecibido, emailRecibido) {
+function validateLogin() {
+
+    // GET INPUT VALUES DEL FORMULARIO DE INICIO DE SESIÓN
+    let ingresePass = document.getElementById("passLogin").value;
+	let ingreseEmail = document.getElementById("emailLogin").value;
 
 	// ACCESO A LAS PROPIEDADES DEL OBJETO USUARIO
 	const email = userData.email;
     const pass = userData.pass;
 
-
 	// COMPARACIÓN ENTRE LOS DATOS DEL USUARIO Y LOS VALORES DE LOS INPUTS DE LOGIN
 	const hayDatosInvalidos = (password, emailUser) => password != pass || emailUser != email;
 
-    if (!hayDatosInvalidos(passRecibido, emailRecibido)) {
+    if (!hayDatosInvalidos(ingresePass, ingreseEmail)) {
     	console.log('Inicio de sesión exitoso');
     } else {
     	console.log('Email o password erroneos');
-		// Intentos
-	    // let intentos = 3;
-	    
-        // while (intentos > 0) {
-	    //   intentos--;
-        //   console.log(`TE QUEDAN ${intentos} INTENTOS`);
-        //   if (intentos == 0) {
-        //   	console.log('Se han acabado los intentos de inicio de sesión, vuelva mas tarde');
-        //   	break;
-        //   }
-        // }
+        if(intentos === 3){
+            console.log('Se han acabado los intentos de inicio de sesión, vuelva mas tarde');
+            // GET POR ID DEL BUTTON 'LOGIN' DEL FORMULARIO DE INICIO DE SESIÓN, QUE SE DESHABILITA
+            // UNA VEZ QUE EL USUARIO AGOTA LOS INTENTOS DE INICIO DE SESIÓN.
+            document.getElementById('btnInputLogin').disabled = 'true';
+        }
+        intentos++;
     }
 }
 
@@ -37,12 +39,7 @@ function validateLogin(passRecibido, emailRecibido) {
 formLogin.addEventListener("submit", (e)=>{
 	e.preventDefault();
 
-	// GET INPUT VALUES DEL FORMULARIO DE INICIO DE SESIÓN
-    let ingresePass = document.getElementById("passLogin").value;
-	let ingreseEmail = document.getElementById("emailLogin").value;
-
     // LLAMADA A LA FUNCIÓN PARA VALIDAR DATOS
-	validateLogin(ingresePass, ingreseEmail);
-	
+	validateLogin();
   }
 )
