e45663a8ebd74d627bc2e6a40876701e90a8c733
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-03 21:57:32 -0300

    fetch json

--- scripts/oferta.js
+++ scripts/oferta.js
@@ -1,5 +1,6 @@
 class Publicacion {
-    constructor(localidad, nombreDelLugar, direccion, descripcion){
+    constructor(puesto, localidad, nombreDelLugar, direccion, descripcion){
+     this.puesto = puesto;
      this.localidad = localidad;
      this.nombreDelLugar = nombreDelLugar;
      this.direccion = direccion;
@@ -30,11 +31,12 @@ const crearPublicacion = (publicacion) => {
     let contenedorPublicacion = document.createElement("div");
     contenedorPublicacion.className = 'ofertaP';
     contenedorPublicacion.innerHTML = `
-                                 <h3>${publicacion.localidad}</h3>  
-                                 <h3>${publicacion.nombreDelLugar}</h3>
-                                 <h3>${publicacion.direccion}</h3>
                                  <p class="text-oferta">
-                                 ${publicacion.descripcion}
+                                  <span>${publicacion.puesto}</span>
+                                  -${publicacion.descripcion}.
+                                  -${publicacion.nombreDelLugar}.
+                                  <span>${publicacion.direccion}</span>
+                                  - <span>${publicacion.localidad}</span>
                                  </p>
                                  `;
     document.querySelector(".listOferta").append(contenedorPublicacion);
@@ -50,12 +52,13 @@ let publicar= document.querySelector("#formulario")
 publicar.addEventListener("submit", (e)=>{
     e.preventDefault();
 
+    let puesto = document.getElementById("puesto").value;
     let localidad = document.getElementById("localidad").value;
     let nombreDelLugar = document.getElementById("nombreDelLugar").value;
     let direccion = document.getElementById("direccion").value;
     let descripcion = document.getElementById("descripcion").value;
 
-    let nuevaPublicacion = new Publicacion(localidad, nombreDelLugar, direccion, descripcion);
+    let nuevaPublicacion = new Publicacion(puesto, localidad, nombreDelLugar, direccion, descripcion);
     listaPublicacion.push(nuevaPublicacion);
     localStorage.setItem("publicacion", JSON.stringify(listaPublicacion));
 
@@ -68,12 +71,6 @@ publicar.addEventListener("submit", (e)=>{
         
         }).showToast();
     console.log(listaPublicacion)
-
-
-
-    
-   
-
 })
 
 // Buscador
@@ -94,7 +91,7 @@ function searchPost(e){
         const postValues = allPosts[i].querySelectorAll('span,p');
         for(let j = 0; j < postValues.length; j++){
             //Check if any word of the post starts with the input search string
-            if((postValues[j].innerHTML.toUpperCase()).includes(searchValue) || (postValues[j].innerHTML.toUpperCase()).includes(searchValue2)  ) {
+            if((postValues[j].innerHTML.toUpperCase()).includes(searchValue) && (postValues[j].innerHTML.toUpperCase()).includes(searchValue2)  ) {
                 count++;
             } 
         }
@@ -110,8 +107,33 @@ function searchPost(e){
 })
 
 
+fetch("../publicacionesOferta.json")
+    .then(response => response.json())
+    .then(result => {
+        const datos = result;
+        datos.forEach(dato => {
+            crearPublicacion(dato)
+        })
+    })
 
 
 
+fetch("../localidades.json")
+.then(response => response.json())
+.then(result => {
+    const datos = result;
+    datos.forEach(dato => {
+        crearLocalidad(dato)
+    })
+})
 
+{/* <option value="Costa Esmeralda">Costa Esmeralda</option> */}
 
+const crearLocalidad = (localidad) => {
+
+    let option = document.createElement("option");
+        option.innerHTML = `
+                              ${localidad.localidad}
+                                     `;
+        document.querySelector("#localidad").append(option);
+}
